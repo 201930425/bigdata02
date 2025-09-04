bf52f3624b7e5fc4468c616c584bcd16bcf7305a
201930425 <012010@email.daelim.ac.kr>
2025-09-04 14:47:17 +0900

    v0.2 numpy

---
+++ week02/week02_numpy.py
@@ -0,0 +1,11 @@
+# 넘파이(Numpy)
+# 써드파티 라이브러리(별도의 설치가 필요, 단 colab에는 기본설치가 되어있음)
+# 파이썬에서 수치 계산을 위한 핵심 라이브러리
+# 다차원 배열 객체와 이를 다루기 위한 함수를 제공
+import numpy as np
+
+#리스트 대신 배열을 사용하는 이유
+list1 = [1, 2.1, "3"]
+print(list1)
+array1 = np.array(list1)
+print(array1)
