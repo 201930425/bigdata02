50b9e1c3e65365839c72119907910efff4cff2e1
201930425 <012010@email.daelim.ac.kr>
2025-09-18 15:55:49 +0900

    v0.9 pandas

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -4,13 +4,18 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-mpg = sns.load_dataset('mpg')
-# print(mpg.value_counts())
+def square(x):
+    return x ** 2
+def cube(x):
+    return x ** 3
 
-df= pd.DataFrame({
-    'city' : ['seoul', 'anyang', 'incheon', 'seoul', 'seoul', 'anyang'],
-    'name' : ['tom', 'jerry', 'kim', 'park', 'park', 'lee']}
-    ,index=[1,2,3,4,5,6])
+mpg = sns.load_dataset('mpg')
+df = pd.DataFrame([
+    [1,4,7],
+    [2,5,8],
+    [3,6,9]],
+    columns=('A','B','C'),
+    index=[1,2,3])
 print(df)
-# print(df.value_counts())
-# print(df['city'].nunique())
+print(df.apply(square))
+print(df.apply(cube))
