b86e791883838ed69857c489fb2682c086c6201a
201930425 <012010@email.daelim.ac.kr>
2025-09-18 16:35:02 +0900

    v1.2 pandas

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -5,10 +5,16 @@ import pandas as pd
 import seaborn as sns
 
 mpg = sns.load_dataset('mpg')
-# print(mpg.isnull().sum())
-# print(mpg.isnull())
-# print(mpg[mpg['horsepower'].isnull() == True])
-# print(mpg[mpg['horsepower'].isnull()])
-mpg_nan = mpg[mpg['horsepower'].isnull()]
-print(mpg_nan[['mpg','horsepower','displacement', 'origin']])
+#
+# mpg_nan = mpg[mpg['horsepower'].isnull()]
+# print(mpg_nan[['mpg','horsepower','displacement', 'origin']])
 
+# 결측치 처리 #1
+mpg.dropna(inplace=True) # mpg = mpg.dropna()
+print(mpg.info())
+# print(mpg[mpg['horsepower'].isnull()])
+
+# 결측치 처리 #2
+# mpg = mpg.fillna(mpg['horsepower'].mean())
+# print(mpg[mpg['horsepower'].isnull()])
+# print(mpg.info())
