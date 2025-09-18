49643613afd592364a76a1fa1bfcdc79574b9d15
201930425 <012010@email.daelim.ac.kr>
2025-09-18 16:45:35 +0900

    v1.3 pandas

--- week04/week04_pandas.py
+++ week04/week04_pandas.py
@@ -10,11 +10,15 @@ mpg = sns.load_dataset('mpg')
 # print(mpg_nan[['mpg','horsepower','displacement', 'origin']])
 
 # 결측치 처리 #1
-mpg.dropna(inplace=True) # mpg = mpg.dropna()
-print(mpg.info())
+# mpg.dropna(inplace=True) # mpg = mpg.dropna()
+# print(mpg.info())
 # print(mpg[mpg['horsepower'].isnull()])
 
 # 결측치 처리 #2
 # mpg = mpg.fillna(mpg['horsepower'].mean())
 # print(mpg[mpg['horsepower'].isnull()])
 # print(mpg.info())
+
+# 결측치 처리 #3
+mpg.drop(columns = ['horsepower'],  inplace=True)
+print(mpg.info())
