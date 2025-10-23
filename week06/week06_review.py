f39ed86305345bbbdda4d0297441789f506269a3
201930425 <012010@email.daelim.ac.kr>
2025-10-23 14:31:16 +0900

    v0.1 python magic_method

--- week06/week06_review.py
+++ week06/week06_review.py
@@ -1,31 +1,8 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
+import numpy as np
+list1 = [1,2,3,4,5]
+array1 = np.array(list1)  # 파이썬 리스트를 넘파이 배열로 변환
+print(list1, type(list1))
+print(array1, type(array1))
 
-df = sns.load_dataset('titanic')
-# print(df.head())
-# print(df.info())
-# print(df.describe())
-
-# plt.figure(figsize = (8,4))
-# sns.histplot(data=df, x='age',bins=16,kde=True)
-# plt.title("Age Disrtibution of Passengers")
-# plt.ylabel('Frequency')
-# plt.xlabel('Age')
-# plt.tight_layout()
-# plt.show()
-
-# plt.figure(figsize = (8,4))
-# sns.countplot(data=df, x='survived')
-# plt.title("Survivors vs Non-Survivors",fontsize=14)
-# plt.xlabel('Survived (0:No, 1:Yes)')
-# plt.ylabel('Count')
-# plt.tight_layout()
-# plt.show()
-
-plt.figure(figsize = (8,4))
-sns.countplot(data=df, x='class', order=['First', 'Second', 'Third'])
-plt.title("Passenger Count by Class",fontsize=14)
-plt.xlabel('class')
-plt.ylabel('Count')
-plt.tight_layout()
-plt.show()
+array2 = np.array([[1,2,3],[4,5,6]])
+print(array2, type(array2))
