c3b8b480b98f809e3c51253b55ed8f231fe1856a
201930425 <012010@email.daelim.ac.kr>
2025-10-30 16:26:45 +0900

    v0.5 visualization

--- week10/week10_pandas.py
+++ week10/week10_pandas.py
@@ -3,10 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-s1 = pd.Series([0,0,1,1,0,1,1,1,0,1,1])
-# print(s1)
-sc1 = s1.cumsum() # cumulative sum
-# print(sc1)
-# print(s1.mul(sc1)) # multiply
-# print(s1.mul(sc1).diff().where(lambda n: n>0).ffill()) # where
-print(s1.mul(sc1).diff().where(lambda n: n>0).ffill().add(sc1,fill_value=0)) # add -
+wh = pd.read_csv('wh.csv')
+# print(wh.head())
+print(wh.describe())
+# print(wh.info())
+sns.scatterplot(x='Weight', y='Height', data=wh)
+plt.show()
