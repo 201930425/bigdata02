5db131a467dd54bd6edc844f4f326a620e925b14
201930425 <012010@email.daelim.ac.kr>
2025-11-06 15:53:32 +0900

    v0.8 visualization

--- week11/week11_sklearn.py
+++ week11/week11_sklearn.py
@@ -12,31 +12,9 @@ diabetes = load_diabetes()
 df_diabetes = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
 df_diabetes['target'] = diabetes.target # 당뇨병 진행 정도 값
 
-fig, axes = plt.subplots(2,2, figsize=(12,8))
-
-axes[0,0].hist(df_diabetes['s1'],bins=20, color='skyblue',edgecolor='black')
-axes[0,0].set_title('Diabetes Data : TC Distribution')
-axes[0,0].set_xlabel('TC')
-axes[0,0].set_ylabel('Frequency')
-
-axes[0,1].boxplot([df_diabetes['s1'],df_diabetes['s2'],df_diabetes['s3']])
-axes[0,1].set_title('Diabetes Data : BoxPlot')
-axes[0,1].set_xticklabels(['TC','LDL','HDL'])
-
-axes[1,0].scatter(df_diabetes['s1'],df_diabetes['target'],color='coral', alpha=0.5)
-axes[1,0].set_title('Diabetes Data : TC vs Progression')
-axes[1,0].set_xlabel('TC')
-axes[1,0].set_ylabel('Progression')
-
-axes[1,1].scatter(df_diabetes['bmi'],df_diabetes['target'],color='green', alpha=0.5)
-axes[1,1].set_title('Diabetes Data : BMI vs Progression')
-axes[1,1].set_xlabel('BMI')
-axes[1,1].set_ylabel('Progression')
-
-plt.show()
-
-sns.histplot(df_diabetes['target'], color='blue', bins=10 ,kde=True)
-plt.title('histogram of Diabetes Progression (target)')
-plt.xlabel('Diabetes Progression')
-plt.ylabel('Frequency')
+corr = df_diabetes.corr() # correlation(상관관계)
+# print(corr)
+#당뇨병 데이터셋 상관관계 히트맵
+plt.figure(figsize = (12,10))
+sns.heatmap(corr,cmap="coolwarm",annot=True,fmt='0.2f',linewidths=.5)
 plt.show()
