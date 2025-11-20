7376fd776dbe4d310ce71d164d6d025e0bb5cd4f
201930425 <012010@email.daelim.ac.kr>
2025-11-20 15:19:02 +0900

    v3.6 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -3,21 +3,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 apple = pd.read_csv('APPL_price.csv')
-# print(apple.head())
-# print(apple.describe())
-# print(apple.info())
 apple['Date'] = pd.to_datetime(apple['Date'])
+apple = apple.set_index('Date')
+
+#일일 수익률 계산(%) 결과 칼럼 추가
+apple['Daily_Return'] = apple['Close'].pct_change() * 100
+# print(apple[['Close', 'Daily_Return']])
+
+#일일 가격 변화 (달러)
+apple['Price_Change'] = apple['Close'].diff()
+# print(apple[['Close', 'Price_Change']])
+
+#일일 가격 변동폭(%)
+apple['High_Low_Range'] = ((apple['High'] - apple['Low']) / apple['Low']) * 100
+# print(apple[['High','Low','High_Low_Range']])
+
+# 이동평균(20)
+apple['MA_20'] = apple['Close'].rolling(window=20).mean()
+# print(apple['MA_20'].tail(20))
+
+# 변동성 (20일 표준편차)
+apple['Volatility'] = apple['Daily_Return'].rolling(window=20).std()
+# print(apple['Volatility'])
+
 # print(apple.info())
-apple = apple.set_index('Date') # index를 날짜 시간컬럼으로 대체
-# print(apple.head())
-# print(apple.tail())
-
-#시계열 데이터 슬라이싱
-# print(apple['2022-06-14':'2022-06-16'])
-# print(apple['2022-04':'2022-06'])
-
-print(apple['Close'])
-apple1m = apple.resample('1m').mean() #일주일 단위로 행을 묶어 평균값 추출
-print(apple1m['Close'])
-# print(apple7days.info())
-# print(apple7days.describe())
+apple = apple.reset_index()
+print(apple[['Date','Close','Daily_Return','MA_20','Volatility']].tail(10))
