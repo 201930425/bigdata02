b182395ba4242e5be4321c3030739fb73e974551
201930425 <012010@email.daelim.ac.kr>
2025-11-20 14:43:32 +0900

    v3.5 pandas

--- week13/week13_pandas.py
+++ week13/week13_pandas.py
@@ -14,4 +14,10 @@ apple = apple.set_index('Date') # index를 날짜 시간컬럼으로 대체
 
 #시계열 데이터 슬라이싱
 # print(apple['2022-06-14':'2022-06-16'])
-print(apple['2022-04':'2022-06'])
+# print(apple['2022-04':'2022-06'])
+
+print(apple['Close'])
+apple1m = apple.resample('1m').mean() #일주일 단위로 행을 묶어 평균값 추출
+print(apple1m['Close'])
+# print(apple7days.info())
+# print(apple7days.describe())
